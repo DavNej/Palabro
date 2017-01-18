1f034a3fe88b998bb7c8ab46d94485cf191158f2
DavNej <david.nejar@gmail.com>
2017-01-18 15:41:20 +0200

    Dav first commit

--- js/palabro.js
+++ js/palabro.js
@@ -1,1 +1,25 @@
-alert('greetings :)')
+var foreignName = [
+    "παλαβρό",
+    "パラブロ",
+    "পালাবার",
+    "ፓላብሮ",
+    "帕拉比如哦",
+    "પલબરો",
+    "פלברו",
+    "بالبر",
+    "पलबरो",
+    "ಪೆಲ್ಯಾಬ್ರೊ",
+    //"പ്ളാബ്രോ",
+    "палабро",
+//    "پالابرو",
+    "पलब्रो"];
+
+var foreignNameString = "<strong>Palabro</strong>";
+
+foreignName.forEach(function(element) {
+    foreignNameString += " | &nbsp; <strong>" + element + "</strong>";
+});
+
+console.log(foreignNameString);
+
+$('#welcome-message').html(foreignNameString);
