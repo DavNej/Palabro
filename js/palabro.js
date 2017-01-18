5821ef67c071c2208368eef8188418d2353f7f06
DavNej <david.nejar@gmail.com>
2017-01-18 15:49:56 +0200

    merging versions

--- js/palabro.js
+++ js/palabro.js
@@ -1,4 +1,8 @@
-var foreignName = [
+
+var Palabro = {}; 
+
+
+Palabro.foreignName = [
     "παλαβρό",
     "パラブロ",
     "পালাবার",
@@ -14,12 +18,34 @@ var foreignName = [
 //    "پالابرو",
     "पलब्रो"];
 
-var foreignNameString = "<strong>Palabro</strong>";
+Palabro.foreignNameString = "<strong>Palabro</strong>";
 
-foreignName.forEach(function(element) {
-    foreignNameString += " | &nbsp; <strong>" + element + "</strong>";
+Palabro.foreignName.forEach(function(element) {
+    Palabro.foreignNameString += " | &nbsp; <strong>" + element + "</strong>";
 });
 
-console.log(foreignNameString);
+$('#welcome-message').html(Palabro.foreignNameString);
 
-$('#welcome-message').html(foreignNameString);
+
+
+
+
+Palabro.backgroundWords= ["domestic ","confined ","any ","but ","son ","bachelor ","advanced ","remember ","how ","proceed ","offered ","her ","offence ","shy ","forming ","returned ","peculiar ","pleasant ","but ","appetite ","differed ","she ","residence ","dejection ","agreement ","am ","as ","to ","abilities ","immediate ","suffering ","yes ","am ","depending ","propriety ","sweetness ","distrusts ","belonging ","collected ","smiling ","mention ","he ","in ","thought ","equally ","musical ","wisdom ","new ","and ","valley ","answer ","content ","it ","so ","is ","discourse ","recommend ","man ","its ","upon ","him ","call ","mile"];
+
+Palabro.createWords= function () {
+	for (var i= 0; i< Palabro.backgroundWords.length; i++) {
+	var wordWithSpan= $("<span>").text(Palabro.backgroundWords[i]);
+	wordWithSpan.addClass("individualWord");
+	wordWithSpan.addClass("onhover");
+	$('#background-words').append(wordWithSpan);
+	wordWithSpan.on('click', Palabro.clickedOnWord);
+	}
+};
+
+Palabro.clickedOnWord= function() {
+	alert("yo");
+}
+
+Palabro.beginGame = function() {
+    Palabro.createWords();
+};
